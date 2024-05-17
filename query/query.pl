238f9edfacdceb4e29b1dc67c37e9a4d6e01cb0d
Radish <53844660+radixsh@users.noreply.github.com>
2024-05-16 17:32:48 -0700

    Write query.pl with help of ChatGPT v4

--- query/query.pl
+++ query/query.pl
@@ -1,29 +1,35 @@
 /* All novels published during the year 1953 or 1996*/
 year_1953_1996_novels(Book) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    novel(Book, Year),
+    (Year = 1953; Year = 1996).
 
 /* List of all novels published during the period 1800 to 1900*/
 period_1800_1900_novels(Book) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    novel(Book, Year),
+    Year >= 1800,
+    Year =< 1900.
 
 /* Characters who are fans of LOTR */
 lotr_fans(Fan) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    fan(Fan, Titles),
+    member(the_lord_of_the_rings, Titles).
 
 /* Authors of the novels owned by Chandler */
 author_names(Author) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    author(Author, Things_written),
+    fan(chandler, Titles),
+    member(Things_written, Titles).
 
 /* Characters who are fans of Brandon Sanderson's novels */
 fans_names(Fan) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    fan(Fan, Titles),
+    (member('mistborn_trilogy', Titles); member('the_way_of_kings', Titles)).
 
 /* Novels common between either of Phoebe, Ross, and Monica */
 mutual_novels(Book) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    (fan(phoebe, P_titles), fan(ross, R_titles), 
+        member(Book, P_titles), member(Book, R_titles));
+    (fan(phoebe, P_titles), fan(monica, M_titles), 
+        member(Book, P_titles), member(Book, M_titles));
+    (fan(ross, R_titles), fan(monica, M_titles), 
+        member(Book, R_titles), member(Book, M_titles)).
