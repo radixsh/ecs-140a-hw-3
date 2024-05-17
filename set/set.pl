5b940b4c879777378f2bf4e320fd472fefc93b1c
Radish <53844660+radixsh@users.noreply.github.com>
2024-05-16 19:19:13 -0700

    Initial set

--- set/set.pl
+++ set/set.pl
@@ -1,11 +1,36 @@
-isUnion(Set1,Set2,Union) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+% File must start with a comment to be interpreted as Prolog not Perl 
+% https://stackoverflow.com/questions/19610734/prolog-support-for-vim-users#19611055
 
-isIntersection(Set1,Set2,Intersection) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+% If Set1 and Set2 are not disjoint sets, then the union will have duplicates.
+isUnion(Set1, Set2, Union) :-
+    append(Set1, Set2, Union).
 
+
+
+% Doesn't detect cases where the 3rd parameter (an array) is missing some
+% elements the sets share.
+isIntersection([], [], []).
+isIntersection(Set1, Set2, Intersection) :- 
+    member(Element, Set1),
+    member(Element, Set2),
+    append([], Element, Intersection).
+% isInBoth(Set1, Set2, T).
+
+% isInBoth(_1, _2, []).
+% isInBoth(Set1, Set2, [H|T]) :-
+%     member(H, Set1),
+%     member(H, Set2),
+%     isInBoth(Set1, Set2, T).
+
+
+
+isEqual([], []).
 isEqual(Set1,Set2) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    length(Set1, Length),
+    length(Set2, Length),
+    contains(Set1, Set2).
+
+contains([], _).
+contains([Set1H | Set1T], Set2) :-
+    member(Set1H, Set2),
+    contains(Set1T, Set2).
