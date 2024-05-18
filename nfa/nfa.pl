ee77453524043bfcc714a47779082946d465be0f
Radish <53844660+radixsh@users.noreply.github.com>
2024-05-18 15:55:19 -0700

    Write nfa.pl

--- nfa/nfa.pl
+++ nfa/nfa.pl
@@ -2,5 +2,6 @@ reachable(StartState, FinalState, []) :-
     StartState = FinalState.
 
 reachable(StartState, FinalState, [InputH | InputT]) :-
-    transition(StartState, NextState, InputH),
-    reachable(NextState, FinalState, InputT).
+    transition(StartState, InputH, NextStateSet),
+    reachable(NextState, FinalState, InputT),
+    member(NextState, NextStateSet).
